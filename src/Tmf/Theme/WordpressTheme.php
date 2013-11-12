2a5d76583dcec8c63378de0af1103966914b4cc6
tmf <tom.forrer@gmail.com>
2013-11-12 15:55:46 +0100

    test template filters

--- src/Tmf/Theme/WordpressTheme.php
+++ src/Tmf/Theme/WordpressTheme.php
@@ -1,15 +1,32 @@
 <?php
 namespace Tmf\Theme;
 
-use Pimple;
+use ServiceContainer;
 
 class WordpressTheme
 {
   public function __construct(ServiceContainer $container)
   {
-    add_filter('index_template', array($this, 'redirectWordpressTemplateParts'));
+    add_action('after_setup_theme', array($this, 'setupTheme'));
   }
-  public function redirectWordpressTemplateParts($part){
+
+  public function setupTheme()
+  {
+    $templateTypes = array('index', 'date', '404', 'search', 'front', 'front_page', 'home', 'archive', 'author', 'category', 'tag', 'taxonomy', 'page', 'paged', 'single', 'attachment', 'comments_popup');
+    foreach ($templateTypes as $type) {
+      add_filter($type . '_template', array($this, 'registerTemplate'));
+    }
+
+    add_filter('template_include', array($this, 'filterTemplateInclude'));
+  }
+
+  public function filterTemplateInclude($file)
+  {
     return false;
+  }
+
+  public function registerTemplate($template)
+  {
+    return $template;
   }
 } 
