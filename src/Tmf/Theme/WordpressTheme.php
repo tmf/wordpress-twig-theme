18224b15c6d2edc1ae8a668556cf8e8b64b5016c
tmf <tom.forrer@gmail.com>
2013-11-12 15:37:21 +0100

    template redirect (simple, first draft)

--- src/Tmf/Theme/WordpressTheme.php
+++ src/Tmf/Theme/WordpressTheme.php
@@ -7,6 +7,9 @@ class WordpressTheme
 {
   public function __construct(ServiceContainer $container)
   {
-
+    add_filter('index_template', array($this, 'redirectWordpressTemplateParts'));
+  }
+  public function redirectWordpressTemplateParts($part){
+    return false;
   }
 } 
