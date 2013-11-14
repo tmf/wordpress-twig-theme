260a4d12be9a3d5de93c8e76c698d64846ce3d0d
tmf <tom.forrer@gmail.com>
2013-11-14 04:35:24 +0100

    vendor autoloader

--- functions.php
+++ functions.php
@@ -1,19 +1,14 @@
 <?php
 
-foreach (array(
-           __DIR__ . '/../../../vendor/',
-           __DIR__ . '/../../vendor/',
-           __DIR__ . '/vendor/'
-         ) as $vendorPath) {
-  if (is_dir(realpath($vendorPath))) {
-    require_once $vendorPath . 'autoload.php';
+$vendors = ABSPATH . '/vendor/autoload.php';
 
-    $container = new Tmf\Theme\ServiceContainer();
+require_once $vendors;
 
-    $wordpressTheme = new Tmf\Theme\WordpressTheme($container);
-    break;
-  }
-}
+$container = new Tmf\Theme\ServiceContainer();
+
+$wordpressTheme = new Tmf\Theme\WordpressTheme($container);
+
+
 
 
 
